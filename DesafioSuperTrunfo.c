e41f57f5c3a0dc78b6290d5f5bbb7f454965c8fc
Leonardo097 <wesleyleo0018@gmail.com>
2025-02-25 23:58:01 +0000

    Desafio Super Trunfo

---
+++ DesafioSuperTrunfo.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+
+char Estado;
+char codigo;
+char cidade;
+int populacao;
+float areakm;
+float pib;
+int pontosTuristicos;
+
+printf("Digite o nome  do Estado: \n");
+scanf("%d", &Estado);
+
+printf("Digite codigo da carta: \n");
+scanf("%d", &codigo);
+
+printf("Digite o nome da Cidade: \n");
+scanf("%d", &cidade);
+
+printf("Digite a População: \n");
+scanf("")
+
+
+
+
+
+}
